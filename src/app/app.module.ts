938c2387afe81dc50602e7dd59520f1e064b6e2e
sanddwich <64834314+sanddwich@users.noreply.github.com>
2020-06-18 12:19:36 +0400

    18-06-2020_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,7 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from './modules/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSliderModule } from '@angular/material/slider';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
+
 
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
@@ -17,7 +11,7 @@ import { HeaderComponent } from './templates/main/header/header.component';
 import { HeaderNavigationComponent } from './templates/main/header-navigation/header-navigation.component';
 import { FooterComponent } from './templates/main/footer/footer.component';
 import { MainComponent } from './pages/main/main.component';
-import { HeaderDrawerComponent } from './templates/header-drawer/header-drawer.component';
+import { HeaderDrawerComponent } from './templates/main/header-drawer/header-drawer.component';
 
 
 @NgModule({
@@ -35,13 +29,6 @@ import { HeaderDrawerComponent } from './templates/header-drawer/header-drawer.c
     AppRoutingModule,
     SharedModule,
     BrowserAnimationsModule,
-    MatMenuModule,
-    MatIconModule,
-    MatSliderModule,
-    MatSidenavModule,
-    MatFormFieldModule,
-    MatSelectModule,
-    MatButtonToggleModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
